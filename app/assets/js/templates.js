3252d023ef8e8684edcda58efdc5b8b4b1734c3a
thorsten zerha <thorsten.zerha@tastenwerk.com>
2015-03-06 09:44:53 +0100

    up

--- app/assets/js/templates.js
+++ app/assets/js/templates.js
@@ -1,5 +1,5 @@
 Ember.TEMPLATES["application"] = Ember.Handlebars.template({"1":function(depth0,helpers,partials,data) {
-  data.buffer.push("<h1>you are logged int</h1>\n");
+  data.buffer.push("<h1>you are logged in</h1>\n");
   },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var stack1, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = '';
   stack1 = helpers['if'].call(depth0, "authenticated", {"name":"if","hash":{},"hashTypes":{},"hashContexts":{},"fn":this.program(1, data),"inverse":this.noop,"types":["ID"],"contexts":[depth0],"data":data});
